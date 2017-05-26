54de6ae93f43fd41dd0c7f7659c2f009add4df5b
John Hall <john.hall@teamaol.com>
2017-05-26 16:17:50 -0400

    added initial swagger

--- jobless/web/app.py
+++ jobless/web/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
 
 from jobless.web.jobs_api import jobs_blueprint
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static')
 app.register_blueprint(jobs_blueprint, url_prefix='/jobs')
 
 
